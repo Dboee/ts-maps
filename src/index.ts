c987312e396c943a641ffa54ffce32cb60d9c20c
Dboee <davidboee@gmail.com>
2022-01-09 13:44:59 +0100

    adds and implements Company class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './classes/User';
+import { Company } from './classes/Company';
 
 console.log('Hello world!');
 const user = new User();
+const company = new Company();
 
 console.log(user);
+console.log(company);
