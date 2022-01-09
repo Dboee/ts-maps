34d85bd54be603ad45cb2e36c216ad4ed6fcfe58
Dboee <davidboee@gmail.com>
2022-01-09 15:26:42 +0100

    Got the local environment api key working in HTML!

--- src/dotenvcfg.ts
+++ src/dotenvcfg.ts
@@ -1,7 +1,3 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
-
-const apiKey = process.env.MAPS_API;
-
-export const apiUrl = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey;
