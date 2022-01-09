53a7e7be416b3dd481c99d782252416ed8ef2209
Dboee <davidboee@gmail.com>
2022-01-09 09:54:48 +0100

    adds random data constuctor to userclass

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -1,7 +1,19 @@
+import faker from 'faker';
+
 class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+
+  constructor() {
+    this.name = faker.name.firstName() + faker.name.lastName();
+    // for the location object, we need to first initialize location,
+    // before we can define latitude and longitude.
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
