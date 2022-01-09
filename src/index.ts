97b3243e0a7434a52363e36443db9438ad4d9756
Dboee <davidboee@gmail.com>
2022-01-09 14:00:29 +0100

    adds dotenv setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './classes/User';
 import { Company } from './classes/Company';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 console.log('Hello world!');
 const user = new User();
