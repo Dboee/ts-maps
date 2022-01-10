fcdf19be48fdec26af3e5099510a4fe9e4d3309a
Dboee <davidboee@gmail.com>
2022-01-10 12:57:52 +0100

    testing that google maps works

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { User } from './classes/User';
-import { Company } from './classes/Company';
+// import { User } from './classes/User';
+// import { Company } from './classes/Company';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log(user);
-console.log(company);
+// console.log(user);
+// console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
