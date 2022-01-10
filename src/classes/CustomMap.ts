2ef985226d611328401e184ff295b47c96443fa5
Dboee <davidboee@gmail.com>
2022-01-10 13:36:02 +0100

    adds addCompanyMarker to CustomMap class

--- src/classes/CustomMap.ts
+++ src/classes/CustomMap.ts
@@ -1,5 +1,7 @@
 // We are making a custom map class from the google maps,
 // to limit access to google maps functions, for safety.
+import { User } from './User';
+import { Company } from './Company';
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -10,6 +12,16 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
       },
     });
   }
